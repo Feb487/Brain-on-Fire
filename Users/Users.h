40799e3f022b18aa808038f64222eecc27aa222b
Giancarlo <giancarlomosca27@outlook.it>
2024-03-14 18:33:37 +0100

    RIMOSSE CARTELLE "Gloabal" e stabilito visione variabili gloabli

--- Users/Users.h
+++ Users/Users.h
@@ -2,12 +2,15 @@
 #define USERS_H_INCLUDED
 
 class Users {
-
+	private:
+	int nomeutente[20];
+	//aggiungi tutte le varibiali globali che servono per la registrazione...
+	
     public:
         void credenzialiRegistrazioneUtente(); //Funzione che acquisisce le credenziali dell'utente per la registrazione
         void credenzialiAccessoUtente(); //Funzione che acquisisce le credenzili dell'utente per l'accesso
-        void registrazione(); //Funzione che registra l'utente in base alle credenziali precedentemente acquisite
-        void accesso(); //Funzione che fa accedere l'utente in base alle credenziali precedentemente acquisite
+        int registrazione(); //Funzione che registra l'utente in base alle credenziali precedentemente acquisite
+        int accesso(); //Funzione che fa accedere l'utente in base alle credenziali precedentemente acquisite
         void salvaEdEsci(); //Funzione che esegue il salvataggio e l'uscita dal gioco
 
 };

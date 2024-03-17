7425742a7ce8032793160bdd0ec89d3f073ab80b
Giancarlo <giancarlomosca27@outlook.it>
2024-03-17 17:10:54 +0100

    modifica fatta alla pull di matteo

--- Banners/Banners.cpp
+++ Banners/Banners.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 void Banners::logoIniziale() {
 
-    //Qui vanno messi i cout che sostituiscono i printf del rispettivo menù (da fare per ogni funzione menù presente nel progetto C precedente)
+    //Qui vanno messi i cout che sostituiscono i printf del rispettivo menï¿½ (da fare per ogni funzione menï¿½ presente nel progetto C precedente)
     cout << "funziona molto bene !" << endl;
 }
 
@@ -14,14 +14,11 @@ void Banners::uscitaFinale() {
 <<<<<<< HEAD
 	
 	cout << "ciao sono matteo" <<endl;
-=======
 
-	cout << "ciao sono giancarlo"<<endl;
->>>>>>> 98dd50d19956abb7810271414b0057cd01450bcc
 }
 
 void Banners::inizioGame(){
 
-    cout << "ciao sono davide e matteo è gay" << endl;
+    cout << "ciao sono davide e matteo ï¿½ gay" << endl;
 	//metti il codice
 }
